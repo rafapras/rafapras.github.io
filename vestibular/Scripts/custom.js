2bec9af887c24d0ebdfd99ee770a494aad798c40
rafapras <45179674+rafapras@users.noreply.github.com>
2018-11-19 16:28:09 -0200

    Add files via upload

---
+++ vestibular/Scripts/custom.js
@@ -0,0 +1,14 @@
+﻿//MOVE TO TOP
+jQuery(document).ready(function (e) {
+    var l = 400,
+        t = 1200,
+        a = 700,
+        i = e(".move-to-top");
+    e(window).scroll(function () {
+        e(this).scrollTop() > l ? i.addClass("cd-is-visible") : i.removeClass("cd-is-visible cd-fade-out"), e(this).scrollTop() > t && i.addClass("cd-fade-out")
+    }), i.on("click", function (l) {
+        l.preventDefault(), e("body,html").animate({
+            scrollTop: 0
+        }, a)
+    })
+});
